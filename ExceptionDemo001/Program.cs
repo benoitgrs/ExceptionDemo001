f151da0112b20cffea4e5fa9be3b8fb7483f7be0
Benoit <24571@student.dorset-college.ie>
2021-11-03 17:14:59 +0000

    Ajoutez des fichiers projet.

---
+++ ExceptionDemo001/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionDemo001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exception demonstration");
+            Divide(6, 3, "answer");
+
+            int[] number = new int[2];
+            try
+            {
+                number[0] = 0;
+                number[1] = 1;
+                number[2] = 2;
+
+                foreach(int i in number)
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+            catch(IndexOutOfRangeException ex)
+            {
+                Console.WriteLine("An IndexOutOfRangeException was thrown");
+                Console.WriteLine(ex.Message);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("An Generale exception was thrown");
+            }
+            finally
+            {
+                Console.WriteLine("Finished process exception thrown have closed all issues");
+            }
+
+            try
+            {
+                Divide(6, 0, "answer");
+            }
+            catch (DivideByZeroException ex)
+            {
+
+            }
+            finally
+            {
+
+            }
+            try
+            {
+                Show(null, 6);
+
+            }
+            catch(ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+
+            }
+        }
+        private static void Divide(int a,int b, string s)
+        {
+            int result = a / b;
+            Console.WriteLine($"{s.ToUpper()}: {result}");
+
+        }
+        private static void Show(string frame, int age)
+        {
+            if(frame is null)
+            {
+
+            }
+
+
+            Console.WriteLine($"{frame} is {age} years old");
+        }
+    }
+}
